f4b3d388504d80eebb8cecaf3e4cbffd69f6e879
ryuu-jarry <157298006+ryuu-jarry@users.noreply.github.com>
2024-01-28 20:37:44 +0530

    Enter roll no.
    mark sheet

---
+++ Student'sMark.py
@@ -0,0 +1,203 @@
+print("Student's Roll No. 101 to 110")
+student = int(input("Enter student's Roll No. :"))
+for rollNo in range(101,111) :
+    if rollNo == student :
+        if rollNo == 101 :
+            print("Student's name is Rahul")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 102 :
+            print("Student's name is Ankur")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 103 :
+            print("Student's name is Mohit")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 104 :
+            print("Student's name is Deepak")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 105 :
+            print("Student's name is Gaurav")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 106 :
+            print("Student's name is Mayank")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 107 :
+            print("Student's name is Parveen")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 108 :
+            print("Student's name is Suraj")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 109 :
+            print("Student's name is Vikas")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
+
+        if rollNo == 110 :
+            print("Student's name is Rohan")
+            hindi = int(input("Enter Hindi marks :"))
+            eng = int(input("Enter English marks :"))
+            ss = int(input("Enter SS marks :"))
+            sci = int(input("Enter Science marks :"))
+            math = int(input("Enter Math marks :"))
+            san = int(input("Enter Sanskrit marks :"))
+            sub = [hindi,eng,ss,sci,math,san]
+            sub = sum(sub) / 6
+            if sub <= 100 and sub >= 75 :
+                Grade = "A"
+            elif sub <= 75 and sub >= 55 :
+                Grade = "B"
+            elif sub <= 55 and sub >= 35 :
+                Grade = "C"
+            else:
+                Grade = "D -> Fail"
+            print("Student's Grade ->",Grade,"Parsentese ->",sub)
